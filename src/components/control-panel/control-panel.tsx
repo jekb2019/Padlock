dce2c46eb470abdf64f1962f1a96bf9e91a4a322
Jason Ko <jekb2019@gmail.com>
2021-05-13 16:38:36 +1200

    Update header design

--- src/components/control-panel/control-panel.tsx
+++ src/components/control-panel/control-panel.tsx
@@ -4,7 +4,7 @@ import styles from './control-panel.module.css';
 const ControlPanel = () => (
         <div className={styles.panel}>
             <button className={styles.btn}>Unlock</button>
-            <button className={styles.btn}><i className="fas fa-cog"></i></button>
+            <button className={styles.btn}><i className="far fa-question-circle"></i></button>
         </div>
     );
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = () => (
-        <h1 className={styles.title}>Digital Padlock</h1>
-    );
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <h1 className={styles.title}>Digital Padlock</h1>
+            <p className={styles.description}>This content has been locked</p>
+        </div>
+    )};
 
 export default Header;
