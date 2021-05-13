a024443d2d07fee189ed9c3cfed2edbd90602d00
Jason Ko <jekb2019@gmail.com>
2021-05-13 16:53:18 +1200

    Add hint info box

--- src/components/control-panel/control-panel.tsx
+++ src/components/control-panel/control-panel.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styles from './control-panel.module.css';
 
-const ControlPanel = () => (
+const ControlPanel = () => {
+
+    // Show Hint for password
+    const showHint = () => {
+        window.alert("Password is #123");
+    }
+    
+    return (
         <div className={styles.panel}>
             <button className={styles.btn}>Unlock</button>
-            <button className={styles.btn}><i className="far fa-question-circle"></i></button>
+            <button onClick={showHint} className={styles.btn}><i className="far fa-question-circle"></i></button>
         </div>
     );
+}
 
 export default ControlPanel;
