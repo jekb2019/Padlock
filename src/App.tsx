6e1c2223a5f1c600848ece37f6241f76b51ecab5
Jason Ko <jekb2019@gmail.com>
2021-05-13 15:21:31 +1200

    Add header UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import Header from './components/header/header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className={styles.app}>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './header.module.css';
+
+const Header = () => (
+        <h1 className={styles.title}>Digital Padlock</h1>
+    );
+
+export default Header;
